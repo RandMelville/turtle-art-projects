fbb90bcc39b9a180840ee28e73864233b0684dd3
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-03-30 17:14:56 -0300

    reactor: refatoração das implementações obtidas
    Foi realizada uma refatoração de todos os códigos obtidos durente pesquisas

--- ArcoIrisMovimento/main.py
+++ ArcoIrisMovimento/main.py
@@ -1,29 +1,8 @@
-import turtle
+from drawer import Drawer
 
-tela = turtle.Screen()
+if __name__ == "__main__":
+    drawer = Drawer(screen_width=600, screen_height=600)
 
-caneta = turtle.Turtle()
-
-
-def desenha_semi_circulo(cor, raio, valor):
-    caneta.color(cor)
-    caneta.circle(raio, -180)
-    caneta.up()
-    caneta.setpos(valor, 0)
-    caneta.down()
-    caneta.right(180)
-
-
-cores = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red']
-
-tela.setup(600, 600)
-
-tela.bgcolor('black')
-
-caneta.right(90)
-caneta.width(10)
-caneta.speed(7)
-
-for i in range(7):
-    desenha_semi_circulo(cores[i], 10 * (i + 8), -10 * (i + 1))
-caneta.hideturtle()
+    while True:
+        drawer.draw_rainbow(rainbow_lines_number=7)
+        drawer.reconfigure_pen()
